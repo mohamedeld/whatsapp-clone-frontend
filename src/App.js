3e105c00d20d40fe00de92214f2aa8f46d100f27
mohamedeld <mohamed.azoz20010@gmail.com>
2023-07-20 18:24:30 +0300

    add redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import {ChatIcon} from "./svg";
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, Login, Register } from "./Pages";
+import { useDispatch } from "react-redux";
+import { logout } from "./features/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
   return (
-    <>
-      <ChatIcon/>
-    </>
+    <div className="dark">
+      <button onClick={()=> dispatch(logout())}>Logout</button>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/Pages/index.js
@@ -0,0 +1,3 @@
+export {default as Home} from './Home';
+export {default as Login} from './LoginPage';
+export {default as Register} from './RegisterPage';
